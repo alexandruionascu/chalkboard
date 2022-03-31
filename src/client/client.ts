11987604edabaa3117e09b38b7c536a4991e9efe
Alexandru Ionascu <alexandruionascu.bd@gmail.com>
2022-03-31 15:16:47 +0300

    boss

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,8 +4,8 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.z = 10;
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000)
+camera.position.z = 10
 
 const renderer = new THREE.WebGLRenderer({
     antialias: true,
@@ -15,8 +15,14 @@ renderer.setPixelRatio(window.devicePixelRatio)
 
 document.body.appendChild(renderer.domElement)
 
-const material = new THREE.MeshPhongMaterial();
+// Create cube and add to scene.
 
+
+const material = new THREE.MeshPhongMaterial()
+
+var geometry = new THREE.BoxGeometry(10, 20, 20, 50, 50, 50);
+mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 const canvas = document.createElement('canvas')
 
@@ -65,6 +71,85 @@ function animate() {
     controls.update()
 
     render()
+}
+
+var drawStartPos = { x: 0, y: 0 }
+var canvasmaterial = new THREE.MeshPhongMaterial()
+
+
+
+setupCanvasDrawing();
+
+function setupCanvasDrawing() {
+    // get canvas and context
+    var drawingCanvas = document.getElementById('drawing-canvas');
+var drawingCanvas2 = document.getElementById('drawing-canvas-2');
+var drawingContext = (drawingCanvas as any).getContext('2d');
+var drawingContext2 = (drawingCanvas2 as any).getContext('2d');
+
+// draw white background
+drawingContext.fillStyle = "#FFFFFF";
+drawingContext.fillRect(0,0,128,128);
+drawingContext2.fillStyle = "#FFFFFF";
+drawingContext2.fillRect(0,0,128,128);
+
+// set canvas as bumpmap
+material.color = new THREE.Color( 1, 1, 1 );
+material.map = new THREE.Texture(drawingCanvas as any);
+
+// set the variable to keep track of when to draw
+var paint = false;
+var paint2 = false;
+
+// add canvas event listeners
+drawingCanvas?.addEventListener('mousedown', function(e){
+  paint = true
+  drawStartPos = {x:e.offsetX, y:e.offsetY};
+});
+drawingCanvas?.addEventListener('mousemove', function(e){
+    if(paint){
+      draw(drawingContext, 0, e.offsetX, e.offsetY);
+  }
+});
+drawingCanvas?.addEventListener('mouseup', function(e){
+  paint = false;
+});
+drawingCanvas?.addEventListener('mouseleave', function(e){
+  paint = false;
+});
+
+drawingCanvas2?.addEventListener('mousedown', function(e){
+  paint2 = true
+  drawStartPos = {x:e.offsetX, y:e.offsetY};
+});
+drawingCanvas2?.addEventListener('mousemove', function(e){
+    if(paint2){
+      draw(drawingContext2, 1, e.offsetX, e.offsetY);
+  }
+});
+drawingCanvas2?.addEventListener('mouseup', function(e){
+  paint2 = false;
+});
+drawingCanvas2?.addEventListener('mouseleave', function(e){
+  paint2 = false;
+});
+}
+
+// Draw function
+function draw(drawContext: any, type: any,  x: number, y: number) {
+drawContext.moveTo(drawStartPos.x, drawStartPos.y);
+if(type){
+  // is displacement
+drawContext.strokeStyle = '#000000';
+}else{
+  // is bump
+drawContext.strokeStyle = '#000000';
+}
+drawContext.lineTo(x,y);
+drawContext.stroke();
+drawStartPos = {x:x, y:y};
+(material as any).map.needsUpdate = true;
+//material.displacementMap.needsUpdate = true;
 }
 
 function render() {
